c061505f506f2a8dfd1b8c18ba6a4f7acc66f70a
Supriya Gorai <115802703+supriya-gorai-p360@users.noreply.github.com>
2026-04-03 17:18:43 -0400

    remove ``

--- app/(tabs)/Info/index.tsx
+++ app/(tabs)/Info/index.tsx
@@ -4,74 +4,74 @@ import { theme } from "@/theme/theme";
 import Header from "@/components/header";
 
 export default function Info() {
-	return (
-		<ScrollView
-			style={{ flex: 1, backgroundColor: theme.colors.bg }}
-			contentContainerStyle={{ backgroundColor: theme.colors.bg }}
-		>
-			<Header />
-			<View style={{ padding: 26 }}>
-				<Text
-					style={{
-						fontSize: 14,
-						fontFamily: theme.typography.fontFamily.medium,
-						color: theme.colors.gray700,
-					}}
-				>
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium
-					sapiente temporibus suscipit eum, consequuntur recusandae quas
-					exercitationem, explicabo beatae architecto, earum ipsam quis alias
-					eaque magni deserunt? Magni, rem voluptates. {`\n \n`}Lorem ipsum
-					dolor sitamet consectetur, adipisicing elit. Laudantium sapiente
-					temporibus suscipit eum, consequuntur recusandae quas exercitationem,
-					explicabo beatae architecto, earum ipsam quis alias eaque magni
-					deserunt? Magni, rem voluptates.
-					{`\n \n`}Lorem ipsum dolor sitamet consectetur, adipisicing elit.
-					Laudantium sapiente temporibus suscipit eum, consequuntur recusandae
-					quas exercitationem, explicabo beatae architecto, earum ipsam quis
-					alias eaque magni deserunt? Magni, rem voluptates.
-				</Text>
-				``
-				<View
-					style={{
-						flexDirection: "row",
-						flexWrap: "wrap",
-						marginTop: 20,
-						gap: 4,
-					}}
-				>
-					<Image
-						source={require("@/assets/images/sponser.png")}
-						style={{ width: 100, height: 100, marginVertical: 20 }}
-						resizeMode="contain"
-					/>
-					<Image
-						source={require("@/assets/images/sponser.png")}
-						style={{ width: 100, height: 100, marginVertical: 20 }}
-						resizeMode="contain"
-					/>
-					<Image
-						source={require("@/assets/images/sponser.png")}
-						style={{ width: 100, height: 100, marginVertical: 20 }}
-						resizeMode="contain"
-					/>
-					<Image
-						source={require("@/assets/images/sponser.png")}
-						style={{ width: 100, height: 100, marginVertical: 20 }}
-						resizeMode="contain"
-					/>
-					<Image
-						source={require("@/assets/images/sponser.png")}
-						style={{ width: 100, height: 100, marginVertical: 20 }}
-						resizeMode="contain"
-					/>
-					<Image
-						source={require("@/assets/images/sponser.png")}
-						style={{ width: 100, height: 100, marginVertical: 20 }}
-						resizeMode="contain"
-					/>
-				</View>
-			</View>
-		</ScrollView>
-	);
+  return (
+    <ScrollView
+      style={{ flex: 1, backgroundColor: theme.colors.bg }}
+      contentContainerStyle={{ backgroundColor: theme.colors.bg }}
+    >
+      <Header />
+      <View style={{ padding: 26 }}>
+        <Text
+          style={{
+            fontSize: 14,
+            fontFamily: theme.typography.fontFamily.medium,
+            color: theme.colors.gray700,
+          }}
+        >
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium
+          sapiente temporibus suscipit eum, consequuntur recusandae quas
+          exercitationem, explicabo beatae architecto, earum ipsam quis alias
+          eaque magni deserunt? Magni, rem voluptates. {`\n \n`}Lorem ipsum
+          dolor sitamet consectetur, adipisicing elit. Laudantium sapiente
+          temporibus suscipit eum, consequuntur recusandae quas exercitationem,
+          explicabo beatae architecto, earum ipsam quis alias eaque magni
+          deserunt? Magni, rem voluptates.
+          {`\n \n`}Lorem ipsum dolor sitamet consectetur, adipisicing elit.
+          Laudantium sapiente temporibus suscipit eum, consequuntur recusandae
+          quas exercitationem, explicabo beatae architecto, earum ipsam quis
+          alias eaque magni deserunt? Magni, rem voluptates.
+        </Text>
+
+        <View
+          style={{
+            flexDirection: "row",
+            flexWrap: "wrap",
+            marginTop: 20,
+            gap: 4,
+          }}
+        >
+          <Image
+            source={require("@/assets/images/sponser.png")}
+            style={{ width: 100, height: 100, marginVertical: 20 }}
+            resizeMode="contain"
+          />
+          <Image
+            source={require("@/assets/images/sponser.png")}
+            style={{ width: 100, height: 100, marginVertical: 20 }}
+            resizeMode="contain"
+          />
+          <Image
+            source={require("@/assets/images/sponser.png")}
+            style={{ width: 100, height: 100, marginVertical: 20 }}
+            resizeMode="contain"
+          />
+          <Image
+            source={require("@/assets/images/sponser.png")}
+            style={{ width: 100, height: 100, marginVertical: 20 }}
+            resizeMode="contain"
+          />
+          <Image
+            source={require("@/assets/images/sponser.png")}
+            style={{ width: 100, height: 100, marginVertical: 20 }}
+            resizeMode="contain"
+          />
+          <Image
+            source={require("@/assets/images/sponser.png")}
+            style={{ width: 100, height: 100, marginVertical: 20 }}
+            resizeMode="contain"
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
 }
